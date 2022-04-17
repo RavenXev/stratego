22fee38ca1d6103485ab79d2ec44b7d44089fcc0
eizasai <eizakuasai@gmail.com>
2022-04-17 00:45:19 -0400

    Update getAvailableMoves.js

--- strategoFunctions-folder/getAvailableMoves.js
+++ strategoFunctions-folder/getAvailableMoves.js
@@ -13,45 +13,47 @@ function getAvailableMoves(rank, position, board) {
     }
     return new_moves
   }
-  function checkclosest(position, board){
-    close_left = position-position%10
-    close_right = position-position%10+9
-    close_down = position%10
+  function checkup(position, board){
     close_up = position%10+90
-    for(index = close_up; index > position; index-10){
-      if (board(index) != null){
+    index = close_up
+    while (index > position){
+      if (board[index] != null){
         close_up = index
       }
+      index -= 10
     }
-    for(index = close_down; index < position; index+10){
-      if (board(index) != null){
-        close_down = index        
+    return close_up
+  }
+  function checkdown(position, board){
+    close_down = position%10
+    index = close_down
+    while (index < position){
+      if (board[index] != null){
+        close_down = index
       }
+      index += 10
     }
-    for(index = close_right; index > position; index--){
-      if (board(index) != null){
+    return close_down
+  }
+  function checkright(position, board){
+    close_right = position-position%10+9
+    for(let index = close_right; index > position; index--){
+      if (board[index] != null){
         close_right = index
       }          
     }
+    return close_right
+  }
+  function checkleft(position, board){
+    close_left = position-position%10
     for(index = close_left; index < position; index++){
-      if (board(index) != null){
+      if (board[index] != null){
         close_left = index
       }                   
     }
-    return close_left, close_down, close_right, close_left
+    return close_left
   }
   current_moves = [position+1,position-1,position+10,position-10]
-  // 42,43,52,53,46,47,56,57 are all banned spaces. The return array
-  // should never contain any of these locations.
-  // obviously, any location below 0 or above 99 should not exist.
-  not_available_spaces = [42,43,52,53,46,47,56,57]
-  for (space of not_available_spaces){
-      for (index = 0; index < current_moves.length; index++){
-        if (current_moves[index] == space || current_moves[index] < 0 || current_moves[index] > 99){
-          current_moves = our_remove(current_moves, current_moves[index])
-      }
-    }
-  }
   // if the rank is 0 (flag) or 99 (bomb), return an empty array.
   if (rank == 0 || rank == 99){
     return []
@@ -77,14 +79,32 @@ function getAvailableMoves(rank, position, board) {
   // Return [0,10,20,30,40,50,60,70,80,90,1,2,3,4,5,6,7,8,9]
   if (rank == 2){
     current_moves = []
-    close_left, close_down, close_right, close_left = checkclosest(position, board)
-    for (index = close_down; index <= close_up; index+10){ //vertical spaces
-      current_moves.push(index)
+    close_left = checkleft(position, board)
+    close_down = checkdown(position, board)
+    close_right = checkright(position, board)
+    close_up = checkup(position, board)
+    index2 = close_down
+    while (index2 < close_up){ //vertical spaces
+      current_moves.push(index2)
+      index2 += 10
     }
     for (index = close_left; index <= close_right; index++){  //horizontal spaces
       current_moves.push(index)
     }
+    current_moves = our_remove(current_moves,position)
   }
+  // 42,43,52,53,46,47,56,57 are all banned spaces. The return array
+  // should never contain any of these locations.
+  // obviously, any location below 0 or above 99 should not exist.
+  not_available_spaces = [42,43,52,53,46,47,56,57]
+  for (space of not_available_spaces){
+    for (index = 0; index < current_moves.length; index++){
+      if (current_moves[index] == space || current_moves[index] < 0 || current_moves[index] > 99){
+        current_moves = our_remove(current_moves, current_moves[index])
+      }
+    }
+  }
+
   // If rank=4 and position = 50
   // Return [40,60,49,51]
   console.log(rank, position)
