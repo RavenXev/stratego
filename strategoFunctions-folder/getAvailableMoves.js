8f5938697d8e85cfec3160b467e483b691cbc958
eizasai <eizakuasai@gmail.com>
2022-04-17 14:22:11 -0400

    Update getAvailableMoves.js

--- strategoFunctions-folder/getAvailableMoves.js
+++ strategoFunctions-folder/getAvailableMoves.js
@@ -2,32 +2,59 @@
  * Returns array of all locations the piece can move to.
  * @param {number} rank rank of the piece.
  * @param {number} position location on the board.
+ * @param {Array} board array of pieces on board
  */
-function getAvailableMoves(rank, position) {
-  function our_remove(current_move,remove){
-    new_array = []
-    for (i of current_move){
-      if (i == remove){
-        console.log('skipped')
-      }
-      else{
-        new_array.push(i)
+function getAvailableMoves(rank, position, board) {
+  function our_remove(current_moves,remove){
+    new_moves = []
+    for (move of current_moves){
+      if (move != remove){
+        new_moves.push(move)
       }
     }
-    return new_array
+    return new_moves
   }
-  current_move = [position+1,position-1,position+10,position-10]
-  // 42,43,52,53,46,47,56,57 are all banned spaces. The return array
-  // should never contain any of these locations.
-  // obviously, any location below 0 or above 99 should not exist.
-  not_available_spaces = [42,43,52,53,46,47,56,57]
-  for (i of not_available_spaces){
-      for (j = 0; j < current_move.length; j++){
-        if (current_move[j] == i || position < 0 || position > 99){
-          current_move = our_remove(current_move, i)
+  function checkup(position, board){
+    close_up = position%10+90
+    index = close_up
+    while (index > position){
+      if (board[index] != null){
+        close_up = index
       }
+      index -= 10
     }
+    return close_up
   }
+  function checkdown(position, board){
+    close_down = position%10
+    index = close_down
+    while (index < position){
+      if (board[index] != null){
+        close_down = index
+      }
+      index += 10
+    }
+    return close_down
+  }
+  function checkright(position, board){
+    close_right = position-position%10+9
+    for(let index = close_right; index > position; index--){
+      if (board[index] != null){
+        close_right = index
+      }          
+    }
+    return close_right
+  }
+  function checkleft(position, board){
+    close_left = position-position%10
+    for(index = close_left; index < position; index++){
+      if (board[index] != null){
+        close_left = index
+      }                   
+    }
+    return close_left
+  }
+  current_moves = [position+1,position-1,position+10,position-10]
   // if the rank is 0 (flag) or 99 (bomb), return an empty array.
   if (rank == 0 || rank == 99){
     return []
@@ -36,54 +63,53 @@ function getAvailableMoves(rank, position) {
   // AKA if the piece is on 0,10,20,30,40,50,60,70,80,90
   // or 9,19,29,39,49,59,69,79,89,99. 
   // for instance, a piece on 9 cannot move to 10 and vice versa.
-  edgeleft = [0,10,20,30,40,50,60,70,80,90]
-  edgeright = [9,19,29,39,49,59,69,79,89,99]
-  for (i of edgeleft){
-    for (j = 0; j < current_move.length; j++){
-      if (current_move[j] == i){
-        current_move = our_remove(current_move,i)
-      }
+  edgeleft = [-1,9,19,29,39,49,59,69,79,89]
+  edgeright = [10,20,30,40,50,60,70,80,90,100]
+  for (space of edgeleft){
+    if (position-1 == space){
+      current_moves = our_remove(current_moves, space)
     }
   }
-  for (i of edgeright){
-    for (j = 0; j < current_move.length; j++){
-      if (current_move[j] == i){
-        current_move = our_remove(current_move, i)
-      }
+  for (space of edgeright){
+    if (position+1 == space){
+      current_moves = our_remove(current_moves, space)
     }
   }
   //Examples
   // If rank=2 and position=0
   // Return [0,10,20,30,40,50,60,70,80,90,1,2,3,4,5,6,7,8,9]
   if (rank == 2){
-    for (i = 0; i < 10; i++){
-      move = i*10+position%10
-      add_move = true
-      for (j = 0; j < current_move.length; j++){
-        if (move == current_move[j]){
-          add_move = false
-        }
-      }
-      if (add_move){
-        current_move.push(move)
-      }
+    current_moves = []
+    close_left = checkleft(position, board)
+    close_down = checkdown(position, board)
+    close_right = checkright(position, board)
+    close_up = checkup(position, board)
+    index2 = close_down
+    while (index2 < close_up){ //vertical spaces
+      current_moves.push(index2)
+      index2 += 10
     }
-    for (i = 0; i < 10; i++){
-      add_move = true
-      for (j = 0; j < current_move.length; j++){
-        if (i == current_move[j]){
-          add_move = false
-        }
-      }
-      if (add_move){
-        current_move.push(i)
+    for (index = close_left; index <= close_right; index++){  //horizontal spaces
+      current_moves.push(index)
+    }
+    current_moves = our_remove(current_moves,position)
+  }
+  // 42,43,52,53,46,47,56,57 are all banned spaces. The return array
+  // should never contain any of these locations.
+  // obviously, any location below 0 or above 99 should not exist.
+  not_available_spaces = [42,43,52,53,46,47,56,57]
+  for (space of not_available_spaces){
+    for (index = 0; index < current_moves.length; index++){
+      if (current_moves[index] == space || current_moves[index] < 0 || current_moves[index] > 99){
+        current_moves = our_remove(current_moves, current_moves[index])
       }
     }
   }
+
   // If rank=4 and position = 50
   // Return [40,60,49,51]
   console.log(rank, position)
-  return current_move
+  return current_moves
 }
 
 export default getAvailableMoves;
