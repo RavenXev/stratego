a4a0f2b5b505499a54d0f54c91a1645196b00523
Evan X <64918172+RavenXev@users.noreply.github.com>
2022-04-21 15:57:05 -0400

    dummyArray on screen

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 import "./style.css";
-import printGameState from "./strategoFunctions-folder/printGameState"
-import dummyArray from "./strategoFunctions-folder/dummyArray.json"
+import printGameState from "./strategoFunctions-folder/printGameState";
+import dummyArray from "./strategoFunctions-folder/dummyArray.json";
 
-printGameState(dummyArray)
+printGameState(dummyArray);
 
-const app = document.getElementById('app')
+const app = document.getElementById("app");
 
-let sampleArray = [
-  {rank:9, position:0, color: "red"}, 
-  {rank:0, position:0, color: "blue"},
-  {rank:2, position:0, color: "red"},
-  {rank:4, position:0, color: "blue"}
-]
-
-sampleArray.forEach((piece) => {
+dummyArray.forEach((piece) => {
   //forEach runs through an array and for each element runs a function
-  const newDiv = document.createElement('div')
-  newDiv.classList.add('newPieceClass')
-  newDiv.style.backgroundColor = piece.color
-  newDiv.textContent= piece.rank
-  app.appendChild(newDiv)
-})
 
+  const newDiv = document.createElement("div"); // use Javascript to make a new div
+  newDiv.classList.add("newPieceClass"); // add a class to it for CSS styling purposes. You can select all of these with ".newPieceClass"
+
+  if (piece != null && piece != "lake") {
+    newDiv.style.backgroundColor = piece.color;
+    newDiv.textContent = piece.rank;
+  }
+  if (piece == null) {
+    newDiv.textContent = "null";
+  }
+  if (piece == "lake") {
+    newDiv.textContent = "lake";
+    newDiv.style.backgroundColor = "#ADD8E6"; // light blue for lakes
+  }
+
+  app.appendChild(newDiv);
+});
